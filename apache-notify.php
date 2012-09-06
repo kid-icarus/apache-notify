18e01a2799aa57472657284af0aa06b5dac21dc3
Ryan Kois <kid_icarus@1482934.no-reply.drupal.org>
2012-09-06 11:37:11 -0600

    Making this more cross-platformy.

--- apache-notify.php
+++ apache-notify.php
@@ -5,7 +5,7 @@ $old_count = 0;
 while ($error_count = count_errors($argv[1])) {
   print $argv[1];
   if ($error_count > $old_count) {
-    system('notify-send "New apache error detected"');
+    send_notification();
     $old_count = $error_count;
   }
   sleep(1);
@@ -15,4 +15,16 @@ function count_errors($filename) {
   $lines_command = "cat $filename | wc -l";
   $lines = system($lines_command);
   return $lines;
+}
+
+function send_notification() {
+  switch (PHP_OS) {
+    case 'Linux':
+      system('notify-send "New apache error detected"');
+      break;
+
+    case 'Darwin':
+      system('growlnotify -s -a terminal -t "Apache Error" -m "New apache error detected"');
+      break;
+  }
 }
